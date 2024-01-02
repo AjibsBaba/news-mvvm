4ae19fd039fcea1dc51580e008da9d967f494f8c
Samuel Ajibade <samuelajibade22@gmail.com>
2024-01-02 17:22:07 +0100

    created data class for posts

---
+++ app/src/main/java/com/ajibsbaba/newsmvvm/data/NewsPost.kt
@@ -0,0 +1,10 @@
+package com.ajibsbaba.newsmvvm.data
+
+data class NewsPost(
+    val id: String,
+    val author: String,
+    val title: String,
+    val description: String,
+    val url: String,
+    val content: String
+)
