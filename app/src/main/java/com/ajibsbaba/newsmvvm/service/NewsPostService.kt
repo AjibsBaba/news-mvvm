e97406e6efe9e3e745d7604926cbde965895c7a2
Samuel Ajibade <samuelajibade22@gmail.com>
2024-01-04 16:16:23 +0100

    modified posts endpoint

--- app/src/main/java/com/ajibsbaba/newsmvvm/service/NewsPostService.kt
+++ app/src/main/java/com/ajibsbaba/newsmvvm/service/NewsPostService.kt
@@ -1,9 +1,10 @@
 package com.ajibsbaba.newsmvvm.service
 
 import com.ajibsbaba.newsmvvm.data.NewsPost
+import com.ajibsbaba.newsmvvm.service.RetrofitInstance.API_KEY
 import retrofit2.http.GET
 
 interface NewsPostService {
-    @GET("everything")
+    @GET("everything?q=tesla&from=2023-12-03&sortBy=publishedAt&apiKey=${API_KEY}")
     suspend fun getNewsPosts(): List<NewsPost>
 }
